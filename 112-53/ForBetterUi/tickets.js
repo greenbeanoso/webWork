4714ee1adae9b1d8e50b26edadb981b86d530ae9
greenbeanoso <osogreenban@gmail.com>
2025-02-07 11:24:56 +0800

    完成拖拉改版(手感腦殘) + 以req提示未輸入

--- 112-53/ForBetterUi/tickets.js
+++ 112-53/ForBetterUi/tickets.js
@@ -14,33 +14,34 @@ function regen() {
 regen()
 $(function () {
 
-    $("#drag div span").draggable({
-        revert: "invalid",
-        containment: "document",
-        cursor: "move"
-    });
+    // $("#drag div span").draggable({
+    //     revert: "invalid",
+    //     containment: "document",
+    //     cursor: "move",
+    // });
 
-    $("#drag-target div").droppable({
-        accept: "#drag div > span",
-        revert: function (event, ui) {
-            sortMove(ui.draggable[0])
-        },
-        drop: function (event, ui) {
-            NUMmove(1, ui.draggable[0])
-        }
-    });
-    $("#drag div").droppable({
-        accept: "#drag-target div > span",
-        revert: function (event, ui) {
-            sortMove(ui.draggable[0])
-        },
-        drop: function (event, ui) {
-            NUMmove(0, ui.draggable[0])
-        }
-    });
-
+    // $("#drag-target div").droppable({
+    //     accept: "#drag div > span",
+    //     drop: function (event, ui) {
+    //         sortMove(ui.helper[0], ui.offset, document.querySelector("#drag-target div"))
+    //     }
+    // });
+    // $("#drag div").droppable({
+    //     accept: "#drag-target div > span",
+    //     drop: function (event, ui) {
+    //         sortMove(ui.helper[0], ui.offset, document.querySelector("#drag div"))
+    //     }
+    // });
+    $("#drag, #drag-target").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+    // $("#drag-target div").sortable({
+    //     // item:"span"
+    // })
 })
-function DoSubmit() {
+
+$("form").submit((e) => {
+    e.preventDefault();
     var Verify = document.querySelectorAll("#drag-target div")
     var last = 0
     var now
@@ -54,61 +55,83 @@ function DoSubmit() {
         }
         last = now
     }
-    var fristName = document.getElementsByName("first_name")[0].value
-    var lastName = document.getElementsByName("last_name")[0].value
-    var phone = document.getElementsByName("phone")[0].value
-    var password = document.getElementsByName("password")[0].value
-    if (fristName == "" || lastName == "" || phone == "" || password == "") {
-        alert("有欄位是空的")
-        regen()
-        return
-    }
-    fetch(`Tickets.php?frist_name=${fristName}
-    &last_name=${lastName}
-    &phone=${phone}
-    &password=${password}`).then((data) => {
-        window.alert("issue tickets successfully");
+
+    document.location.href = "home.html";
+    console.log("你已送出表單")
+    fetch(`Tickets.php?frist_name=${$("input[name=first_name]").val()}
+        &last_name=${$("input[name=last_name]").val()}
+        &phone=${$("input[name=phone]").val()}
+        &password=${$("input[name=password]").val()}`).then((data) => {
+
         document.location.href = "home.html";
+        window.alert("issue tickets successfully");
     })
+})
+
+
+function DoSubmit() {
+    // var Verify = document.querySelectorAll("#drag-target div")
+    // var last = 0
+    // var now
+    // for (let j = 0; j < Verify.length; j++) {
+    //     now = parseInt(Verify[j].textContent, 10)
+    //     // console.log(last, now)
+    //     if (now < last || Verify.length != 4) {
+    //         alert("驗證碼錯誤")
+    //         regen()
+    //         return
+    //     }
+    //     last = now
+    // }
+
+    // var fristName = document.getElementsByName("first_name")[0].value
+    // var lastName = document.getElementsByName("last_name")[0].value
+    // var phone = document.getElementsByName("phone")[0].value
+    // var password = document.getElementsByName("password")[0].value
+
+    // if (fristName == "" || lastName == "" || phone == "" || password == "") {
+    //     alert("有欄位是空的")
+    //     regen()
+    //     return
+    // }
+    // fetch(`Tickets.php?frist_name=${fristName}
+    // &last_name=${lastName}
+    // &phone=${phone}
+    // &password=${password}`).then((data) => {
+    //     window.alert("issue tickets successfully");
+    //     document.location.href = "home.html";
+    // })
 
 }
-function NUMmove(dis, ui) {
-    console.log(ui)
-    ui.remove()
+function sortMove(ui, position, Tel) {
+    console.log(Tel.children)
+    var len = Tel.children.length
+    var Rmax = 1040
+    var Lmax = 880
+    var low
+    var up
     ui.style.left = 0
     ui.style.top = 0
     ui.style.right = 0
     ui.style.bottom = 0
-    if (dis) {
-        target.append(ui)
-    } else {
-        document.querySelector("#drag div").append(ui)
-    }
-}
-function sortMove(ui) {
-    console.log("hi")
-    var BestSC = 0
-    var best
-    for (const child of ui.parentElement.children) {
-        if (BestSC < getPosition(child)[x] - getPosition(ui)[x] + getPosition(child)[y] - getPosition(ui)[y]) {
-            BestSC = getPosition(child)[x] - getPosition(ui)[x] + getPosition(child)[y] - getPosition(ui)[y]
-            best = child
+    for (let i = 0; i < len; i++) {
+        if (Lmax + (Rmax - Lmax) / len * i < position["left"]) {
+            low = i
+
+        } else if (Lmax + (Rmax - Lmax) / len * i > position["left"]) {
+            up = i
+            break
         }
-    }
-    if (getPosition(best)[x] > getPosition(ui)) {
-        best.after(ui)
-    }
-}
-function getPosition(element) {
 
-    var x = 0;
-    var y = 0;
-    // 搭配上面的示意圖可比較輕鬆理解為何要這麼計算
-    while (element) {
-        x += element.offsetLeft - element.scrollLeft + element.clientLeft;
-        y += element.offsetTop - element.scrollLeft + element.clientTop;
-        element = element.offsetParent;
     }
-
-    return { x: x, y: y };
+    if (low == undefined && up == undefined) {
+        Tel.append(ui)
+        console.log("無", low, up)
+    } else if (low == undefined) {
+        Tel.prepend(ui)
+        console.log("前", low, up)
+    } else {
+        Tel.children[low].after(ui)
+        console.log("差 or 後", low, up)
+    }
 }
